ce98b89cc7ba801dcb7ff53c7f4ccf01e22935f7
GopalSabale <gopalsabale4@gmail.com>
2026-02-11 16:13:35 +0530

    new

---
+++ src/main/java/com/example/mybookplans/MybookplansApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybookplans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookplansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybookplansApplication.class, args);
+	}
+
+}
